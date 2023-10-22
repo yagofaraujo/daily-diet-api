5e466bc6877c1241cdfa962097822508b6120cdb
Yago MacOS <yagoferreira556@gmail.com>
2023-10-22 11:08:55 -0300

    feat: add create user controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './infra/http/app.controller';
+import { AppService } from './infra/http/app.service';
 import { PrismaService } from './infra/prisma/prisma.service';
+import { CreateUserController } from './infra/http/controllers/create-user.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, CreateUserController],
   providers: [AppService, PrismaService],
 })
 export class AppModule {}
---
+++ src/infra/http/controllers/create-user.controller.ts
@@ -0,0 +1,9 @@
+import { Controller, Post } from '@nestjs/common';
+
+@Controller('/users')
+export class CreateUserController {
+  @Post()
+  async handle() {
+    console.log('chama criação de usuário');
+  }
+}
