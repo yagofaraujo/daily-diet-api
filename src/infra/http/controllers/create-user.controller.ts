ca80deb21b808129ae2d44d99bdcd4c47c33eebe
Yago Araújo <yagoferreira556@gmail.com>
2023-10-24 18:13:41 -0300

    feat: add create user validation and zod validation pipe

--- src/infra/http/controllers/create-user.controller.ts
+++ src/infra/http/controllers/create-user.controller.ts
@@ -1,20 +1,34 @@
 import { CreateUserUseCase } from '@/domain/usecases/create-user';
 import { StudentAlreadyExistsError } from '@/domain/usecases/errors/student-already-exists-error';
-import { Body, ConflictException, Controller, Get, HttpCode, InternalServerErrorException, Post } from '@nestjs/common';
+import {
+  Body,
+  ConflictException,
+  Controller,
+  HttpCode,
+  InternalServerErrorException,
+  Post,
+  UsePipes,
+} from '@nestjs/common';
+import { z } from 'zod';
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe';
+
+const createUserBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+});
+
+type CreateUserBodySchema = z.infer<typeof createUserBodySchema>;
 
 @Controller('/users')
 export class CreateUserController {
   constructor(private createUserUseCase: CreateUserUseCase) {}
 
-  @Get()
-  async test() {
-    return 'oi';
-  }
-
   @Post()
   @HttpCode(201)
-  async handle(@Body() body: any) {
-    const { name, email, password } = body;
+  @UsePipes(new ZodValidationPipe(createUserBodySchema))
+  async handle(@Body() body: CreateUserBodySchema) {
+    const { name, email, password } = createUserBodySchema.parse(body);
 
     const result = await this.createUserUseCase.execute({
       name,
