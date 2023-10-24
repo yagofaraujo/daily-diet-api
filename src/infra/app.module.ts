2cc995aba1ec3d5caf1d59f6da29763bc9d3183e
Yago Araújo <yagoferreira556@gmail.com>
2023-10-24 18:25:52 -0300

    chore: add config module to validate env variables

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { HttpModule } from './http.module';
+import { envSchema } from './env';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    HttpModule,
+  ],
 })
 export class AppModule {}
