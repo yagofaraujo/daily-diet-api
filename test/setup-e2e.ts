eb7f90d767732fe195c7a1360bb0ce473c6efdfc
Yago Araújo <yagoferreira556@gmail.com>
2023-10-25 17:03:15 -0300

    chore: config setup to create a new schema to apply tests

---
+++ test/setup-e2e.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from '@prisma/client';
+import { execSync } from 'child_process';
+import { randomUUID } from 'crypto';
+import 'dotenv/config';
+
+const prisma = new PrismaClient();
+
+const SCHEMA_ID = randomUUID();
+
+function generateUniqueDatabaseURL(schemaId: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Provide a DATABASE_URL environment variable.');
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+
+  url.searchParams.set('schema', schemaId);
+
+  return url.toString();
+}
+
+beforeAll(async () => {
+  const databaseUrl = generateUniqueDatabaseURL(SCHEMA_ID);
+
+  process.env.DATABASE_URL = databaseUrl;
+
+  console.log(process.env.DATABASE_URL);
+
+  execSync('npx prisma migrate deploy');
+});
+
+afterAll(async () => {
+  await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${SCHEMA_ID}" CASCADE`);
+  await prisma.$disconnect();
+});
