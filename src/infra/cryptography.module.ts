74ef56661a6fba3d0b3307306253cd86f6ad11cd
Yago Araújo <yagoferreira556@gmail.com>
2023-10-25 19:22:55 -0300

    chore: remove injectable from crptography service

--- src/infra/cryptography.module.ts
+++ src/infra/cryptography.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { BcryptHasher } from './cryptography/bcrypt-hasher';
-// import { IHashGenerator } from '@/domain/usecases/contracts/cryptography/hash-generator';
 
 @Module({
   providers: [
